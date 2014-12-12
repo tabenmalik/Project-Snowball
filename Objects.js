ac969852229340c81c51e576dd0547f37453b26b
Bodaciouslycrazy <bodiemalik@gmail.com>
2014-12-11 23:35:56 -0600

    adding

--- Objects.js
+++ Objects.js
@@ -3,7 +3,7 @@ function Ball(a,b,c){//x,y coordinate and radius
 	this.y = b;
 	this.r = c; //radius
 	
-	function fa(num){//distance (in a random diraction)
+	function fa(num){//distance (in a random direction)
 		var angle = (Math.random() * 2 * Math.PI) - Math.PI;
 		this.x += Math.sin(angle) * num;
 		this.y += Math.cos(angle) * num;
