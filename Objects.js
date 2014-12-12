30328c8cb9f53cbbc2662de809c77079c61d5959
Bodaciouslycrazy <bodiemalik@gmail.com>
2014-12-11 23:29:31 -0600

    BallObject

---
+++ Objects.js
@@ -0,0 +1,13 @@
+function Ball(a,b,c){//x,y coordinate and radius
+	this.x = a;
+	this.y = b;
+	this.r = c; //radius
+	
+	function fa(num){//distance (in a random diraction)
+		var angle = (Math.random() * 2 * Math.PI) - Math.PI;
+		this.x += Math.sin(angle) * num;
+		this.y += Math.cos(angle) * num;
+	}
+	
+	this.moveRandom = fa;
+}
